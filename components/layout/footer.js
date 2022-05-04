ba0a4e1bbd1ae7208f8b0c8cd700664c38eafa9b
Matt Watson <mwatson@renci.org>
2022-05-04 19:19:44 -0400

    add borders for layout debugging

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -6,16 +6,28 @@ export const Footer = () => {
   const theme = useTheme()
   return (
     <Box
-      component="footer"
       sx={{
-        background: 'transparent',
-        padding: '1rem',
+        background: '#ddd',
+        padding: '2rem',
         color: theme.palette.text.primary,
+        '& .link-list': {
+          border: '2px dashed blue',
+          padding: 0,
+          listStyleType: 'none',
+          '& li': {
+            marginBottom: '1rem',
+          }
+        },
       }}
     >
       <Container maxWidth="lg">
-        <Grid container>
-          <Grid item xs={ 12 } md={ 6 } lg={ 5 }>
+        <Grid container sx={{
+          border: '1px dashed crimson',
+          '& > *': {
+            border: '1px dotted purple'
+          }
+        }}>
+          <Grid item xs={ 12 } md={ 12 } lg={ 5 }>
             <Box>
               <Box component="span" sx={{
                 display: 'block',
@@ -35,30 +47,30 @@ export const Footer = () => {
             <p>media@renci.org</p>
             <p>919-445-9640</p>
           </Grid>
-          <Grid item xs={ 12 } md={ 6 } lg={ 3 }>
+          <Grid item xs={ 12 } md={ 4 } lg={ 3 }>
             <strong>Partners</strong>
-            <p>
-              <Link to="https://www.unc.edu/">UNC-Chapel Hill</Link> <br/>
-              <Link to="https://www.ncsu.edu/">NC State University</Link> <br/>
-              <Link to="https://duke.edu/">Duke University</Link>
-            </p>
+            <ul className="link-list">
+              <li><Link to="https://www.unc.edu/">UNC-Chapel Hill</Link></li>
+              <li><Link to="https://www.ncsu.edu/">NC State University</Link></li>
+              <li><Link to="https://duke.edu/">Duke University</Link></li>
+            </ul>
           </Grid>
-          <Grid item xs={ 12 } md={ 6 } lg={ 2 }>
+          <Grid item xs={ 12 } md={ 4 } lg={ 2 }>
             <strong>Connect</strong>
-            <p>
-              <Link to="https://www.twitter.com/RENCI">Twitter</Link> <br/>
-              <Link to="https://www.facebook.com/renci.org">Facebook</Link> <br/>
-              <Link to="https://www.linkedin.com/company/65321">LinkedIn</Link> <br/>
-              <Link to="https://www.youtube.com/RENCIMedia">YouTube</Link> <br/>
-            </p>
+            <ul className="link-list">
+              <li><Link to="https://www.twitter.com/RENCI">Twitter</Link></li>
+              <li><Link to="https://www.facebook.com/renci.org">Facebook</Link></li>
+              <li><Link to="https://www.linkedin.com/company/65321">LinkedIn</Link></li>
+              <li><Link to="https://www.youtube.com/RENCIMedia">YouTube</Link></li>
+            </ul>
           </Grid>
-          <Grid item xs={ 12 } md={ 6 } lg={ 2 }>
+          <Grid item xs={ 12 } md={ 4 } lg={ 2 }>
             <strong>More</strong>
-            <p>
-              <Link to="/about">About</Link> <br/>
-              <Link to="/careers">Careers</Link> <br/>
-              <Link to="https://dashboard.renci.org/">RENCI Dashboard</Link> <br/>
-            </p>
+            <ul className="link-list">
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/careers">Careers</Link></li>
+              <li><Link to="https://dashboard.renci.org/">RENCI Dashboard</Link></li>
+            </ul>
           </Grid>
           <Grid item xs={ 12 } style={{ textAlign: 'center' }}>
             &copy; { new Date().getFullYear() }
