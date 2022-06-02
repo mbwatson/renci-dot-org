c101d03ecd4d16ad3ba8dd686c546aeea8b9258b
Suejin Kim <kim.suejin.20@gmail.com>
2022-06-02 16:24:39 -0400

    add person card to teams pages and adjust query to include photo

--- pages/teams/[id].js
+++ pages/teams/[id].js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 import { fetchTeam } from '../../lib/contentful'
 import { Page } from '../../components'
 import { Pre } from '../../components/pre'
+import { PersonCard } from '../../components/person-card'
 
 export default function ResearchGroup() {
   const router = useRouter()
@@ -27,10 +28,19 @@ export default function ResearchGroup() {
       description={ team.description }
       heroImage={ team.featuredImage ? team.featuredImage.url : null }
     >
-      <Pre>
-        { JSON.stringify(team, null, 2) }
-      </Pre>
-
+      <Typography paragraph>{team.description}</Typography>
+      <br/>
+      <Box sx={{
+          flex: 1,
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+        }}>
+        {
+          team.teamMembersCollection.items.map(person => (
+            <PersonCard key={ person.slug } person={ person } />
+          ))
+        }
+      </Box>
     </Page>
   )
 }
