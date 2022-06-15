b6bde7568fa595eb680cbdd8aa0ddd321fb0d3a3
Matt Watson <mwatson@renci.org>
2022-06-15 11:14:58 -0400

    start chief scientists view

---
+++ pages/chief-scientists.js
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+import { Page } from '../components'
+
+export default function ChiefScientists() {
+  return (
+    <Page
+      title="Chief Scientists"
+    >
+      <Typography>
+        Given RENCI’s emphasis on multidisciplinary and collaborative
+        approaches to advancing scientific discovery, we partner with
+        experts who hold primary appointments in other departments to
+        develop mutually beneficial research collaborations. Our Chief
+        Scientists offer premier expertise in their specializations and
+        participate in existing projects as well as funding proposals. 
+      </Typography>
+    </Page>
+  )
+}
