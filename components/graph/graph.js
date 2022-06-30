7f24ba752b5e7b0b9f9c1cb5b03bc1c211c370c2
Matt Watson <mwatson@renci.org>
2022-06-30 17:17:28 -0400

    set up loadable for graph import

--- components/graph/graph.js
+++ components/graph/graph.js
@@ -1,7 +1,29 @@
-import ForceGraph2D from 'react-force-graph-2d'
+import loadable from '@loadable/component'
+import { Box } from '@mui/material'
+
+const ForceGraph2D = loadable(() => import('./graph-import'))
 
 export const Graph = () => {
+  const graphData = {
+    nodes: [{ id: 1 }, { id: 2 }],
+    links: [{ source: 1, target: 2 }]
+  }
+
   return (
-    <div>graph</div>
+    <Box sx={{
+      border: '1px solid #ddd',
+      '& .force-graph-container': {
+        border: '1px solid #ddd',
+        '& canvas': {
+          border: '1px solid #dd0',
+        },
+      },
+    }}>
+      <ForceGraph2D
+        height={ 400 }
+        width={ 400 }
+        graphData={ graphData }
+      />
+    </Box>
   )
 }
