3778562a809fe91c5c7b631f9f0a92f4b7293511
Matt Watson <mwatson@renci.org>
2022-06-30 16:26:29 -0400

    add divider between title and content in Section

--- components/layout/section/section.js
+++ components/layout/section/section.js
@@ -1,25 +1,33 @@
 import PropTypes from 'prop-types'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, useTheme } from '@mui/material'
 
 export const Section = ({ title, children }) => {
+  const theme = useTheme()
   return (
     <Grid
       container
       columns={ 8 }
-      columnSpacing={{ xs: 0, md: 2 }}
       rowSpacing={{ xs: 2, md: 10 }}
       sx={{
         margin: '3rem 0',
-        '& .title': {
-          xs: { textAlign: 'left' },
-          md: { textAlign: 'right' },
+        '& .title-container': {
+          borderRight: `1px solid ${ theme.palette.grey[200] }`,
+          height: '100%',
+          paddingRight: '1rem',
+          '& .title': {
+            xs: { textAlign: 'left' },
+            md: { textAlign: 'right' },
+          },
+        },
+        '& .content-container': {
+          paddingLeft: '1rem',
         },
       }}
     >
       <Grid item xs={ 8 } md={ 3 }>
         {
           title && (
-            <Box>
+            <Box className="title-container">
               <Typography variant="h3" className="title">
                 { title }
               </Typography>
@@ -28,7 +36,7 @@ export const Section = ({ title, children }) => {
         }
       </Grid>
       <Grid item xs={ 8 } md={ 5 }>
-        <Box>
+        <Box className="content-container">
           { children }
         </Box>
       </Grid>
