a149f11a60ae00a66da7b348b03a0f2e6565f1ed
Suejin Kim <kim.suejin.20@gmail.com>
2022-11-16 16:13:20 -0500

    remove unnecessary files

--- pages/api/staff-photos.js
+++ pages/api/staff-photos.js
@@ -1,21 +1,21 @@
-import { fetchGraphQL } from '../../lib/contentful'
+// import { fetchGraphQL } from '../../lib/contentful'
 
-export default function handler(req, res) {
-  return fetchGraphQL(`query {
-      personCollection {
-        items {
-          pid
-          photo {
-            url
-          }
-        }
-      }
-    }`).then(response => {
-      const payload = response.data.personCollection.items.reduce((obj, { pid, photo }) => {
-        return ({ ...obj, [pid]: photo?.url || null })}, {})
-      res.status(200).json(payload)
-    }).catch(error => {
-      res.status(500).json(error)
-    })
-  fetchData()
-}
+// export default function handler(req, res) {
+//   return fetchGraphQL(`query {
+//       personCollection {
+//         items {
+//           pid
+//           photo {
+//             url
+//           }
+//         }
+//       }
+//     }`).then(response => {
+//       const payload = response.data.personCollection.items.reduce((obj, { pid, photo }) => {
+//         return ({ ...obj, [pid]: photo?.url || null })}, {})
+//       res.status(200).json(payload)
+//     }).catch(error => {
+//       res.status(500).json(error)
+//     })
+//   fetchData()
+// }
