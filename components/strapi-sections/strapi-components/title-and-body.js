5ad058fcee5a8bee8364efe7379857f2fec6ba91
Suejin Kim <kim.suejin.20@gmail.com>
2023-01-06 14:03:24 -0500

    add key to iterator

--- components/strapi-sections/strapi-components/title-and-body.js
+++ components/strapi-sections/strapi-components/title-and-body.js
@@ -2,7 +2,7 @@ import { Section } from '../../layout';
 
 export const TitleAndBody = ({ data }) => {
   return data.sectionContent.map((section) => (
-    <Section title={ section.leftTitle }>
+    <Section title={ section.leftTitle } key={section.id + section.leftTitle}>
       { section.rightBody }
     </Section>
   ))
