b7721194a34596fbb3725b447e3d74989995357b
Matt Watson <mwatson@renci.org>
2023-01-06 14:21:12 -0500

    add comment

--- components/markdown.js
+++ components/markdown.js
@@ -1,7 +1,13 @@
 import ReactMarkdown from 'react-markdown'
 import { Link } from './link'
 
+// this object defines a map:
+//    DOM elements --> React components.
+// this allows us to streamline styles for content coming
+// from Strapi with that of content built here by Nextjs.
+
 const componentMap = {
+  // for links, we'll use our smart link component.
   a: ({ node, href, ...props }) => (
     <Link
       to={ href }
