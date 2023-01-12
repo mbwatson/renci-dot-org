ea53cf20e08255a3f511931b125e7a11d395c6f9
Suejin Kim <kim.suejin.20@gmail.com>
2023-01-12 13:08:36 -0500

    define styles for markdown text elements, bumped h1's to h3, h2's to h4, etc to match heading order on the page

--- components/markdown.js
+++ components/markdown.js
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material'
 import ReactMarkdown from 'react-markdown'
 import { Link } from './link'
 
@@ -13,6 +14,21 @@ const componentMap = {
       to={ href }
       { ...props }
     />
+  ),
+  p: ({ node, children, ...props })=>(
+    <Typography paragraph {...props}>{children}</Typography>
+  ),
+  h1: ({ node, children, ...props }) => (
+    <Typography variant="h3" {...props}>{children}</Typography>
+  ),
+  h2: ({ node, children, ...props }) => (
+    <Typography variant="h4" {...props}>{children}</Typography>
+  ),
+  h3: ({ node, children, ...props }) => (
+    <Typography variant="h5" {...props}>{children}</Typography>
+  ),
+  ul: ({ node, children, ...props }) => (
+    <ul style={{marginTop: 0, lineHeight: '1.8'}} {...props}>{children}</ul>
   ),
 }
 
