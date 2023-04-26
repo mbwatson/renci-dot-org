740f8c8497424214a3d8e94994926621c60b357a
David Glymph <Woozl@users.noreply.github.com>
2023-04-26 15:12:10 -0400

    make access token public for clients to use

--- utils/api.js
+++ utils/api.js
@@ -10,7 +10,7 @@ export async function fetchAPI(path, options = {}) {
     headers: {
       "Content-Type": "application/json",
       Authorization:
-          `Bearer ${process.env.ACCESS_TOKEN}`,
+          `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
     },
   };
   const mergedOptions = {
