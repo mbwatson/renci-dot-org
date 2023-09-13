291ee55f6cf3a825bbecd5d84d35f52f974ddcc5
suejinkim20 <kim.suejin.20@gmail.com>
2023-09-13 09:14:10 -0400

    add in placeholder article navigation

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -6,7 +6,58 @@ import { Markdown } from '../../components/markdown'
 import {ArticleDate} from '../../components/news/article-date'
 import { Label, Tag } from '../../components/news/tag'
 
+const ArticleNavigation = ({ prev, next }) => {
+  return (
+    <div style={{ display: "flex", margin: "1.5rem auto" }}>
+      <div style={{ flex: 1, textAlign: "left" }}>
+        {prev && (
+          <Link to={prev}>
+            PREVIOUS{" "}ARTICLE
+            <br />
+            <Typography variant="caption">{prev}</Typography>
+          </Link>
+        )}
+      </div>
+      <div style={{ flex: 1, textAlign: "right" }}>
+        {next && (
+          <Link to={next}>
+            NEXT{" "}ARTICLE
+            <br />
+            <Typography variant="caption">{next}</Typography>
+          </Link>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const sampleSlugs = [
+  "exogeni-a-critical-step-forward-for-edge-cloud-computing",
+  "nrig-director-ilya-baldin-inducted-into-the-nc-state-computer-science-alumni-hall-of-fame",
+  "renci-internship-program-investing-in-the-next-generation-of-leaders",
+  "renci-researchers-awarded-2021-best-paper-from-the-elsevier-fgcs-journal",
+  "drone-projects-take-data-processing-and-communication-to-new-heights",
+  "biomedical-data-translator-platform-moves-to-the-next-phase",
+  "new-data-format-aids-large-scale-evolutionary-biology-research",
+  "new-streamlined-statistical-method-provides-improved-pattern-detection-and-risk-prediction-for-disease",
+  "use-cases-show-translators-potential-to-expedite-clinical-research",
+  "new-concept-poised-to-accelerate-drug-discovery-through-data-mining",
+]
+
+const createPrevNext = (sampleSlugs, articleSlug) => {
+  const slugIndex = sampleSlugs.indexOf(articleSlug)
+  const prevSlugIndex = slugIndex - 1
+  const nextSlugIndex = prevSlugIndex + 2
+  const prev = sampleSlugs[prevSlugIndex]
+  const next = sampleSlugs[nextSlugIndex]
+
+  return { "prev" : prev, "next": next}
+
+}
+
 export default function NewsArticle({ article }) {
+  const {prev, next} = createPrevNext(sampleSlugs, article.slug)
+
   return (
       <Page title={article.title} hideTitle>
         <Section>
@@ -48,7 +99,9 @@ export default function NewsArticle({ article }) {
       </Section>
 
       <Divider/>
-      <p>Previous Article</p>
+
+      <ArticleNavigation prev={prev} next={next} />
+
       </Page>
     )
 }
