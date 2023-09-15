7f253db00b729518d5356707769d757001fe135f
Matt Watson <mwatson@renci.org>
2023-09-15 13:02:29 -0400

    fix toggle button colors

--- components/news/filters/type-select.js
+++ components/news/filters/type-select.js
@@ -30,21 +30,34 @@ export const TypeSelect = () => {
       onChange={ handleChange }
       aria-label="article type"
       sx={{
+        '.MuiToggleButton-root': {
+          color: '#33445599',
+          transition: 'color 250ms',
+          '&:hover': {
+            color: '#334455',
+          }
+        },
         '.Mui-selected': {
           color: color,
           backgroundColor: `${ color }33`,
         },
       }}
     >
-      <ToggleButton value="all" aria-label="all">
-        All News
-      </ToggleButton>
-      <ToggleButton value="blog" aria-label="blog">
-        Blog
-      </ToggleButton>
-      <ToggleButton value="feature" aria-label="feature">
-        Features
-      </ToggleButton>
+      <ToggleButton
+        value="all"
+        aria-label="all"
+        disabled={ filters.type === 'all' }
+      >All News</ToggleButton>
+      <ToggleButton
+        value="blog"
+        aria-label="blog"
+        disabled={ filters.type === 'blog' }
+      >Blog</ToggleButton>
+      <ToggleButton
+        value="feature"
+        aria-label="feature"
+        disabled={ filters.type === 'feature' }
+      >Features</ToggleButton>
     </ToggleButtonGroup>
   )
 }
