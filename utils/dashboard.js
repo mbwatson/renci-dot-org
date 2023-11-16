aedbec52c3f4dc870fb84cee7b2175e2bff337cf
Sergio Rocha <srocha@renci.org>
2023-11-16 07:17:46 -0600

    pushing up new dashbooard api utils

---
+++ utils/dashboard.js
@@ -0,0 +1,27 @@
+export function getDashboardURL(endpoint) {
+  return `${
+    process.env.NEXT_PUBLIC_DASHBOARD_API_URL ||
+    "http://dashboard.sandy-web.ad.renci.org/api/webinfo"
+  }${endpoint}`;
+}
+
+// Helper to make GET requests to Dashboard
+export async function fetchFromDashboard(endpoint, options = {}) {
+  const defaultOptions = {
+    headers: {
+      accept: "application/json",
+    },
+  };
+  const mergedOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+  const requestUrl = getDashboardURL(endpoint);
+  const response = await fetch(requestUrl, mergedOptions);
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error(`uh oh.. no data was received`);
+  }
+  const data = await response.json();
+  return data;
+}
