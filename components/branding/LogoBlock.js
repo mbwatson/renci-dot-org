43d5579cb9ea6822da032c31194bbd05b2fcbe39
suejinkim20 <kim.suejin.20@gmail.com>
2024-02-07 18:37:09 -0500

    update styles for logo block and remove image url section

--- components/branding/LogoBlock.js
+++ components/branding/LogoBlock.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, CardContent } from '@mui/material/'
 import { InfoBlock } from './InfoBlock'
 import Image from 'next/image'
+import {Link} from '../link'
 
 const styles = {
   card: {
@@ -11,45 +12,26 @@ const styles = {
   topper: {
     borderBottom: `1px solid grey`,
   },
-  image: {
-    width: '100%',
-  },
   content: {
     backgroundColor: 'white',
   },
-  info: {
-
-  }
 }
-
-
-// const filenameFromPath = (path) => {
-//     const imagePathRegex = /^[\/\w]+\/(.+)\..+(\.\w+)/
-//     const match = path.match(imagePathRegex)
-//     return (match && match.length > 0) ? match.slice(1,3).join() : path
-// }
 
 export const LogoBlock = ({logo, bgColor}) => {
   return (
     <Card sx={ styles.card }>
       <CardContent sx={ styles.topper } style={{ backgroundColor: bgColor }}>
-        <Image 
-          src={ logo.src } 
-          alt="Logo" 
-          width={logo.width}
-          height={logo.height}
-          layout="responsive"
-          objectFit='cover'
-          // sx={ styles.image }
-        />
+        <Link to={logo.src}>
+          <Image 
+            src={ logo.src } 
+            alt="Logo" 
+            width={logo.width}
+            height={logo.height}
+            layout="responsive"
+            objectFit='cover'
+          />
+        </Link>
       </CardContent>
-      <CardContent sx={ styles.content }>
-        <InfoBlock
-          title="Image"
-          body={ logo.src }
-          style={styles.info}
-        />                
-      </CardContent> 
     </Card>
   )
 }
