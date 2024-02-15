e05696731dfb64ed4f97a38bfd4f770b585d5b1b
suejinkim20 <kim.suejin.20@gmail.com>
2024-02-15 13:48:40 -0500

    move hexToRgb function into utils

--- components/branding/ColorBlock.js
+++ components/branding/ColorBlock.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import { Card, CardContent, Stack } from '@mui/material/'
 import { InfoBlock } from './InfoBlock'
-
-const hexToRgb = (hex) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-  } : null
-}
+import { hexToRgb } from '@/utils/hextoRgb'
 
 const styles = {
  card: {
